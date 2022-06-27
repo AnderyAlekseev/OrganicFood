497802cb7d32125fc856faa06c953bf73bf9553d
Andrey Alekseev <AnderyAlekseev@ya.ru>
2022-06-27 17:10:46 +0700

    Поправил адаптив хедера

--- dist/php/config.php
+++ dist/php/config.php
@@ -1,15 +1,15 @@
 <?php
 // для OpenServer
- $db_host =  'localhost'; 
- $db_name =  'organicfruit';  
- $db_username = 'admin_OrganicFruit'; 
- $db_password = '';     
+//  $db_host =  'localhost'; 
+//  $db_name =  'organicfruit';  
+//  $db_username = 'admin_OrganicFruit'; 
+//  $db_password = '';     
 
 
  // для хостинга
-//  $db_host =  'localhost'; 
-//  $db_name =  'aleks116_organicfruit'; 
-//  $db_username = 'aleks116_admin_organicfruit';
-//  $db_password = 'fZ7-zTT-Bzg-ChP';    
+ $db_host =  'localhost'; 
+ $db_name =  'aleks116_organicfruit'; 
+ $db_username = 'aleks116_admin_organicfruit';
+ $db_password = 'fZ7-zTT-Bzg-ChP';    
 
 ?>
